45f9f7fa6f1903d55e334d1ac8dfb28fff9a4914
ArielBonfil <61304733+ArielBonfil@users.noreply.github.com>
2020-02-21 02:08:21 -0300

    Add files via upload

---
+++ TobsExamen/assets/script.js
@@ -0,0 +1,13 @@
+
+
+function change() {
+
+    document.getElementById('label1').innerHTML
+        = '$139';
+}
+
+function change2() {
+
+    document.getElementById('label1').innerHTML
+        = '$39';
+}
