4a092b84ceb1b0c1a23a827745c10d79101a323b
ArielBonfil <61304733+ArielBonfil@users.noreply.github.com>
2020-02-21 02:47:14 -0300

    Add files via upload

--- TobsExamen/assets/script.js
+++ TobsExamen/assets/script.js
@@ -4,10 +4,30 @@ function change() {
 
     document.getElementById('label1').innerHTML
         = '$139';
+        document.getElementById('label2').innerHTML
+        = '/Year';
+        document.getElementById('label3').innerHTML
+        = '$149';
+        document.getElementById('label4').innerHTML
+        = '/Year';
+        document.getElementById('label5').innerHTML
+        = '$159';
+        document.getElementById('label6').innerHTML
+        = '/Year';
 }
 
 function change2() {
 
     document.getElementById('label1').innerHTML
         = '$39';
+        document.getElementById('label2').innerHTML
+        = '/Monthly';
+        document.getElementById('label3').innerHTML
+        = '$49';
+        document.getElementById('label4').innerHTML
+        = '/Monthly';
+        document.getElementById('label5').innerHTML
+        = '$59';
+        document.getElementById('label6').innerHTML
+        = '/Monthly';
 }
